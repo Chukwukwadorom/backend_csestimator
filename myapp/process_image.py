7b9ac6192b8275cd6a29451d57da1e89546cd5a2
Chukwukwadorom <chikwadonwattah@gmail.com>
2025-08-21 19:49:07 +0100

    the one i updated image processing

--- myapp/process_image.py
+++ myapp/process_image.py
@@ -1,18 +1,40 @@
+from django.conf import settings
+from tensorflow import keras
+from dotenv import load_dotenv
 import tensorflow as tf
+import os
 # from tensorflow.keras.preprocessing import image
 import numpy as np
 import pandas as pd
 import cv2
+import boto3
+from io import BytesIO
 
 
-import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_PATH = os.path.join(BASE_DIR, 'revised_best_model.h5')
-
-model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+MODEL_PATH = "/tmp/my_model.h5"
 
 
+# Create S3 client (it will automatically pick up keys from env)
+
+BUCKET=settings.AWS_STORAGE_BUCKET_NAME
+KEY=settings.S3_MODEL_KEY
+
+def get_model(BUCKET,KEY):
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading model from S3...")
+        s3 = boto3.client("s3")
+        obj = s3.get_object(Bucket=BUCKET, Key=KEY)
+        bytestream = BytesIO(obj['Body'].read())
+
+        with open(MODEL_PATH, "wb") as f:
+            f.write(bytestream.getbuffer())
+
+    print("Loading model from local cache...")
+    return keras.models.load_model(MODEL_PATH, compile=False)
+
+
+model = get_model(BUCKET,KEY)
 
 
 
@@ -32,5 +54,6 @@ Columns = [
 ]
 def predict_sizes(front_img, side_img):
     predicted_measurements = model.predict([front_img, side_img])
+    # predicted_measurements = np.round(predicted_measurements, 1)
     predicted_df = pd.DataFrame(predicted_measurements, columns=Columns)
     return predicted_df
