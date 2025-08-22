a252253a15a9873d70b65b9bae8f1eaf3ea1861e
Chukwukwadorom <chikwadonwattah@gmail.com>
2025-08-22 18:01:27 +0100

    the one where i updated how the model loads

--- myapp/process_image.py
+++ myapp/process_image.py
@@ -16,25 +16,41 @@ MODEL_PATH = "/tmp/my_model.h5"
 
 
 # Create S3 client (it will automatically pick up keys from env)
-
 BUCKET=settings.AWS_STORAGE_BUCKET_NAME
 KEY=settings.S3_MODEL_KEY
 
-def get_model(BUCKET,KEY):
-    if not os.path.exists(MODEL_PATH):
-        print("Downloading model from S3...")
-        s3 = boto3.client("s3")
-        obj = s3.get_object(Bucket=BUCKET, Key=KEY)
-        bytestream = BytesIO(obj['Body'].read())
+# def get_model(BUCKET,KEY):
+#     if not os.path.exists(MODEL_PATH):
+#         print("Downloading model from S3...")
+#         s3 = boto3.client("s3")
+#         obj = s3.get_object(Bucket=BUCKET, Key=KEY)
+#         bytestream = BytesIO(obj['Body'].read())
 
-        with open(MODEL_PATH, "wb") as f:
-            f.write(bytestream.getbuffer())
+#         with open(MODEL_PATH, "wb") as f:
+#             f.write(bytestream.getbuffer())
 
-    print("Loading model from local cache...")
-    return keras.models.load_model(MODEL_PATH, compile=False)
+#     print("Loading model from local cache...")
+#     return keras.models.load_model(MODEL_PATH, compile=False)
 
 
-model = get_model(BUCKET,KEY)
+_model = None
+
+def get_model(BUCKET, KEY):
+    global _model
+    if _model is None:
+        if not os.path.exists(MODEL_PATH):
+            print("Downloading model from S3...")
+            s3 = boto3.client("s3")
+            obj = s3.get_object(Bucket=BUCKET, Key=KEY)
+            bytestream = BytesIO(obj['Body'].read())
+
+            with open(MODEL_PATH, "wb") as f:
+                f.write(bytestream.getbuffer())
+
+        print("Loading model from local cache...")
+        _model = keras.models.load_model(MODEL_PATH, compile=False)
+    return _model
+
 
 
 
@@ -53,6 +69,7 @@ Columns = [
     "shoulderToCrotch", "thigh", "waist", "wrist"
 ]
 def predict_sizes(front_img, side_img):
+    model = get_model(BUCKET,KEY)
     predicted_measurements = model.predict([front_img, side_img])
     # predicted_measurements = np.round(predicted_measurements, 1)
     predicted_df = pd.DataFrame(predicted_measurements, columns=Columns)
